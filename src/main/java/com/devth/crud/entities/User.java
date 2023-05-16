a5d5d1871f16282390bdbb4aa6b8aff05360ddca
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-05-16 15:05:25 -0300

    User entity ORM

--- src/main/java/com/devth/crud/entities/User.java
+++ src/main/java/com/devth/crud/entities/User.java
@@ -1,8 +1,19 @@
 package com.devth.crud.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
 	private String tel;
